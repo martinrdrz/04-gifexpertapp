699bcb87c07503567fee48d9cee15bf537eb9e41
martinrdrz <martinrdrz@hotmail.com>
2024-05-13 18:48:47 -0300

    agregado de estados y submit

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,21 +1,29 @@
 import { useState } from 'react';
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One Punch', 'Secnod Punch']);
-    console.log(categories);
+    
+    const onAddCategory = () => {
+      //console.log("valorant");
+      setCategories([...categories, "Valorant"]);
+    };
+
     return (
-        <>
-            {/*titulo */}
-            <h1>GifExpertApp</h1>
+      <>
+        {/*titulo */}
+        <h1>GifExpertApp</h1>
 
-            {/*input */}
+        {/*input */}
+        <AddCategory />
 
-            {/*listado de Gif */}
-            <ol>
-                {categories.map((category) => {
-                    return <li key={category}>{category}</li>;
-                })}
-            </ol>
-        </>
+        {/*listado de Gif */}
+        <button onClick={onAddCategory}>Add Category</button>
+        <ol>
+          {categories.map((category) => {
+            return <li key={category}>{category}</li>;
+          })}
+        </ol>
+      </>
     );
 };
