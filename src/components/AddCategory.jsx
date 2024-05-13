8e9b4ecf9fd35e514f1df5b083cab455850559cc
martinrdrz <martinrdrz@hotmail.com>
2024-05-13 18:49:41 -0300

    agregado de un comentario

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -11,12 +11,13 @@ export const AddCategory = () => {
     event.preventDefault();
     console.log(inputValue);
   };
-  // return (
-  //   <input type="text" placeholder="Buscar gifs" value={inputValue} onChange={(event) => [onInputChange(event)]} />
-  // );
+  
   return (
     <form onSubmit={(event) => onSubmit(event)}>
       <input type="text" placeholder="Buscar gifs" value={inputValue} onChange={onInputChange} />;
     </form>
   );
 };
+
+//Recordar que la linea de abajo no es necesario enviar el parametro event, ayque por defecto JavaScript lo hace solo cuando se tiene un solo parametro. Al no colocar ese parametro no significa que no s e envia, sino que JavaScript lo envia igual, por eso enel codigode arriba se puede minimizar dicha instrucción
+//onChange={(event) => [onInputChange(event)]} />
