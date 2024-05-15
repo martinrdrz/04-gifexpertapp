2ea036f3e0bd0be9f5025f1bf41b99e232ca11ad
martinrdrz <martinrdrz@hotmail.com>
2024-05-15 18:27:22 -0300

    se agregaron algunas pruebas unitarias

--- src/components/GitItem.jsx
+++ src/components/GitItem.jsx
@@ -1,8 +1,15 @@
+import { PropTypes } from 'prop-types';
+
 export const GitItem = ({ title, url }) => {
     return (
-        <div className="card">
+        <div className='card'>
             <img src={url} alt={title} />
             <p>{title}</p>
         </div>
     );
+};
+
+GitItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.isRequired,
 };
